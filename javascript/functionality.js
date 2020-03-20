66c26ce874317dd855aec2fda00211e80acda9be
Ahmed Samir <ahmedsamir96m@gmail.com>
2020-03-20 14:04:13 +0200

    Update functionality.js

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -24,6 +24,7 @@ const removeNote = id => {
 // Remove all the Notes from the localStorage and clear the notes list HTML
 const removeNotes = (notes) => {
     window.localStorage.removeItem('notes');
+    saveNotes(notes)
     document.querySelector('#notes').innerHTML = '';
 }
 
